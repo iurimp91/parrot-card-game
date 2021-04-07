861b0860937ff63819d98db49b62c675b5c74cc0
Iuri Magnago <iurimp91@gmail.com>
2021-04-07 19:33:11 -0300

    Adicionada funcionalidade de virar cartas + colocar pares de cartas diferentes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,14 +7,39 @@ function abrirPagina() {
 
     const mesa = document.querySelector("ul");
 
-    for (let i = 0; i < numeroCartas; i++) {
-        mesa.innerHTML += `<li><img src="media/front.png" alt="Papagaio"></li>`;
+    if (numeroCartas === 4) {
+        for (let i = 0; i < 2; i++) {
+            mesa.innerHTML += `
+                <li onclick='virarCarta(this)'>
+                <img class="para-baixo" src="media/front.png" alt="Papagaio">
+                <img class="para-cima escondido" src="media/explodyparrot.gif" alt="Papagaio que explode">
+                </li>
+            `;
+        }
+        for (let i = 0; i < 2; i++) {
+            mesa.innerHTML += `
+                <li onclick='virarCarta(this)'>
+                <img class="para-baixo" src="media/front.png" alt="Papagaio">
+                <img class="para-cima escondido" src="media/bobrossparrot.gif" alt="Papagaio que explode">
+                </li>
+            `;
+        }
     }
 
-
-
-
-
+    let cartas = [];
+    cartas = mesa.querySelectorAll("ul li");
 }
 
-//abrirPagina();
+function virarCarta(elemento) {
+
+
+    const cartaFrente = elemento.querySelector(".para-baixo");
+    cartaFrente.classList.add("escondido");
+
+    const cartaCostas = elemento.querySelector(".para-cima");
+    cartaCostas.classList.remove("escondido");
+}
+
+
+
+abrirPagina();
